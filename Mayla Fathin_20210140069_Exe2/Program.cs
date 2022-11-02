b8161f6f934f65cee1ec65f89e89ba8a9ee6a429
TA1069 <maylafaat@gmail.com>
2022-11-02 17:06:40 +0700

    final commit

---
+++ Mayla Fathin_20210140069_Exe2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mayla_Fathin_20210140069_Exe2
+{
+    1233
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
